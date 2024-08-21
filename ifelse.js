f11b631aa015c2750707df29aed9f8c45d5e4ae1
duuaaa03 <duajaved527@gmail.com>
2024-08-22 00:36:08 +0500

    first commit

---
+++ ifelse.js
@@ -0,0 +1,50 @@
+// if (hour < 18) {
+//     greeting = "Good day";
+//   } else {
+//     greeting = "Good evening";
+//   }
+
+
+// example 1
+// if (Grade < B) {
+//     remarks = "Welldone!";
+//   } else {
+//     remarks = "Work hard";
+//   }
+
+// example 2
+// if (Marks > 50) {
+//     Grade = "A+";
+//   } else {
+//     Grade = "B";
+//   }
+
+// example 3
+
+if (number > 0){
+    print("The number is positive.");
+}else{
+    print("The number is non-positive.")
+}
+    // example 4
+
+if (age >= 18){
+    print("You are eligible to vote.")
+}else{
+    print("You are not eligible to vote.")
+}
+
+// example 5
+if (temperature > 25){
+    print("It's a hot day.");
+}else{
+    print("It's a cold day.")
+}
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/duuaaa03/javascript.git
+// git push -u origin main
---
+++ unaryoperator.js
@@ -0,0 +1,92 @@
+// let a = 5;
+// a++;
+// console.log(a);
+// --a;
+// console.log(a);
+// let b = 4;
+// --b;
+// console.log(b);
+// b++;
+// console.log(b);
+// let c;
+// c= a++ + --a + b++ + --b + b;
+// console.log(c);
+
+
+// Example 1
+
+// let  A =10;
+// A++;
+// console.log(A);
+// --A;
+// console.log(A);
+
+// let B=7;
+// --B;
+// console.log(B);
+// B++;
+// console.log(B);
+
+// let C=6;
+// C++;
+// console.log(C);
+// --C;
+// console.log(C);
+// let x;
+// x=A++ + B++ + --B + C++;
+// console.log(x);
+
+// // example 2
+// let  d =3;
+// d++;
+// console.log(d);
+// --d;
+// console.log(d);
+
+// let h=7;
+// --h;
+// console.log(h);
+// h++;
+// console.log(h);
+
+// let m=6;
+// m++;
+// console.log(m);
+// --m;
+// console.log(m);
+
+// let n=3;
+// ++n;
+// console.log(n);
+// --n;
+// console.log(n);
+// let p=1;
+// ++p;
+// console.log(p);
+// --p;
+// console.log(p);
+// let y;
+// y=d++ + h++ + --h + m++ + --n + p++;
+// console.log(y);
+
+// example 4
+
+let w=15;
+w++;
+console.log(w);
+--w;
+console.log(w);
+let q=25;
+q++;
+console.log(q);
+--q;
+console.log(q);
+let r=20;
+r++;
+console.log(r);
+--r;
+console.log(r);
+let z;
+z=w++ + --w + --q + r++ + --r;
+console.log(z);
+
